afd9d3dde8a9e8cf2b03d3ed5527f225edd81f81
Rahul Sharma <rahul.sharmaa9001@gmail.com>
2025-02-28 16:30:54 +0530

    Implemented Global Exceptional Handler for Handling Exceptions in Stock Management

---
+++ src/main/java/com/rahulsharma/BookingInventry/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.rahulsharma.BookingInventry.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(InsufficientStockException.class)
+    public ResponseEntity<String> handleInsufficientStock(InsufficientStockException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
